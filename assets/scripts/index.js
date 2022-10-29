1a25f8baba6259c2687d129ee729e2a766b528aa
Alena Nemtsova <jelenatbs@gmail.com>
2022-10-29 17:15:41 +0300

    js code fixes

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,21 +1,21 @@
 
 // счетчик обратного времени
-timeend = new Date();
+let timeend = new Date();
 timeend = new Date(timeend.getFullYear() + 1, 0, 1);
 
 function time() {
-    today = new Date();
+    let today = new Date();
     today = Math.floor((timeend - today) / 1000);
     today = Math.floor(today / 60);
     // if (tsec < 10)
     //     tsec = '0' + tsec;
-    tmin = today % 60;
+    let tmin = today % 60;
     today = Math.floor(today / 60);
     if (tmin < 10)
         tmin = '0' + tmin;
-    thour = today % 24;
+    let thour = today % 24;
     today = Math.floor(today / 24);
-    timestr = today + " дней " + thour + " часов " + tmin + " минут ";
+    let timestr = today + " дней " + thour + " часов " + tmin + " минут ";
     document.getElementById('time').innerHTML = timestr;
     window.setTimeout(time, 1000);
 }
